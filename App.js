40f086933c7d396bb6734d4680ffab704279418c
Javiuty <javiiescuadra@gmail.com>
2020-10-09 13:17:19 +0200

    adding some style

--- App.js
+++ App.js
@@ -5,21 +5,57 @@ import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 const App = () => {
   return (
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <View style={styles.container}>
-        <Text>Hello, world!</Text>
-      </View>
+     <View styles={styles.container}>
+       <Text style={styles.text}>Here are some boxes of different color</Text>
+       <View style={[styles.box, styles.cyan]}>
+         <Text style={styles.boxText}>Cyan: #2aa198</Text>
+       </View>
+       <View style={[styles.box, styles.blue]}>
+         <Text style={styles.boxText}>Blue: #268bd2</Text>
+       </View>
+       <View style={[styles.box, styles.magenta]}>
+         <Text style={styles.boxText}>Magenta: #d33682</Text>
+       </View>
+       <View style={[styles.box, styles.orange]}>
+         <Text style={styles.boxText}>Orange: #cb4b16</Text>
+       </View>
+     </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 10,
-    paddingVertical: 30,
-    backgroundColor: "teal",
-    borderColor: "pink",
-    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingTop: 40,
   },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  box: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
 
 export default App;
