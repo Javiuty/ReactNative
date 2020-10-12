f84dd4086e0ffefc2b39c697907952de6c75c73f
Javiuty <javiiescuadra@gmail.com>
2020-10-12 16:19:05 +0200

    react navigation

--- App.js
+++ App.js
@@ -2,37 +2,42 @@ import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { View, Text, SafeAreaView, StyleSheet, FlatList } from "react-native";
 import ColorBox from "./components/ColorBox";
-
+import { NavigationContainer } from "@react-navigation/native";
 
 const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
+  { colorName: "Base03", hexCode: "#002b36" },
+  { colorName: "Base02", hexCode: "#073642" },
+  { colorName: "Base01", hexCode: "#586e75" },
+  { colorName: "Base00", hexCode: "#657b83" },
+  { colorName: "Base0", hexCode: "#839496" },
+  { colorName: "Base1", hexCode: "#93a1a1" },
+  { colorName: "Base2", hexCode: "#eee8d5" },
+  { colorName: "Base3", hexCode: "#fdf6e3" },
+  { colorName: "Yellow", hexCode: "#b58900" },
+  { colorName: "Orange", hexCode: "#cb4b16" },
+  { colorName: "Red", hexCode: "#dc322f" },
+  { colorName: "Magenta", hexCode: "#d33682" },
+  { colorName: "Violet", hexCode: "#6c71c4" },
+  { colorName: "Blue", hexCode: "#268bd2" },
+  { colorName: "Cyan", hexCode: "#2aa198" },
+  { colorName: "Green", hexCode: "#859900" },
 ];
 
 const App = () => {
   return (
-    <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <FlatList
-        style={styles.container}
-        data={COLORS}
-        keyExtractor={item => item.colorName}
-        renderItem={({ item }) => <ColorBox colorName={item.colorName} hexCode={item.hexCode} />}
-       /> 
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={GlobalStyles.droidSafeArea}>
+        <FlatList
+          style={styles.container}
+          data={COLORS}
+          keyExtractor={(item) => item.colorName}
+          renderItem={({ item }) => (
+            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+          )}
+          ListHeaderComponent={<Text style={styles.text}></Text>}
+        />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
@@ -46,6 +51,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 10,
   },
- });
+});
 
 export default App;
