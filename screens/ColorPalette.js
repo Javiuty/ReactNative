f4ba46b851be3d74ec983d485323271977182154
Javiuty <javiiescuadra@gmail.com>
2020-10-13 16:02:49 +0200

    fetching data, use effect, use state

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -1,54 +1,34 @@
 import React from "react";
-import { FlatList, StyleSheet, Text } from 'react-native';
-import ColorBox from '../components/ColorBox';
-
-
-  const RAINBOW = [
-    { colorName: 'Red', hexCode: '#FF0000' },
-    { colorName: 'Orange', hexCode: '#FF7F00' },
-    { colorName: 'Yellow', hexCode: '#FFFF00' },
-    { colorName: 'Green', hexCode: '#00FF00' },
-    { colorName: 'Violet', hexCode: '#8B00FF' },
-  ];
-  
-  const FRONTEND_MASTERS = [
-    { colorName: 'Red', hexCode: '#c02d28' },
-    { colorName: 'Black', hexCode: '#3e3e3e' },
-    { colorName: 'Grey', hexCode: '#8a8a8a' },
-    { colorName: 'White', hexCode: '#ffffff' },
-    { colorName: 'Orange', hexCode: '#e66225' },
-  ];
+import { FlatList, StyleSheet, Text } from "react-native";
+import ColorBox from "../components/ColorBox";
 
 const ColorPalette = ({ route }) => {
-
-    const { colors, paletteName } = route.params;
-
+  const { colors, paletteName } = route.params;
 
   return (
-      <FlatList
-        style={styles.container}
-        data={colors}
-        keyExtractor={(item) => item.colorName}
-        renderItem={({ item }) => (
-          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-        )}
-        ListHeaderComponent={<Text style={styles.text}>{paletteName}</Text>}
-      />
- 
+    <FlatList
+      style={styles.container}
+      data={colors}
+      keyExtractor={(item) => item.colorName}
+      renderItem={({ item }) => (
+        <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+      )}
+      ListHeaderComponent={<Text style={styles.text}>{paletteName}</Text>}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      paddingHorizontal: 10,
-      paddingTop: 10,
-      backgroundColor: 'white',
-    },
-    text: {
-      fontSize: 18,
-      fontWeight: "bold",
-      marginBottom: 10,
-    },
-  });
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
 
 export default ColorPalette;
--- screens/Home.js
+++ screens/Home.js
@@ -1,36 +1,47 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-
-
-const SOLARIZED = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-  ];
+import React, { useCallback, useEffect, useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
+import PalettePreview from "./PalettePreview";
 
 const Home = ({ navigation }) => {
+  const [colorPalettes, setColorPalettes] = useState([]);
+
+  const fetchColorPalettes = useCallback(async () => {
+    const result = await fetch(
+      "https://color-palette-api.kadikraman.now.sh/palettes"
+    );
+
+    if (result.ok) {
+      const palettes = await result.json();
+      setColorPalettes(palettes);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchColorPalettes();
+  }, []);
+
   return (
-    <View>
-      <TouchableOpacity onPress={() => {
-          navigation.navigate('ColorPalette', { paletteName: 'Solarized', colors: SOLARIZED, })
-      }}>
-        <Text>Solarized</Text>
-      </TouchableOpacity>
-    </View>
+    <FlatList
+      style={styles.list}
+      data={colorPalettes}
+      keyExtractor={(item) => item.paletteName}
+      renderItem={({ item }) => (
+        <PalettePreview
+          handlePress={() => {
+            navigation.navigate("ColorPalette", item);
+          }}
+          colorPalette={item}
+        />
+      )}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  list: {
+    padding: 10,
+    backgroundColor: "white",
+  },
+});
 
 export default Home;
